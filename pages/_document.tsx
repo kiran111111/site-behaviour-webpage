72d06a57e84729abdad4f4a3a57d507618e8144f
Kirandeep <kiran111candy@gmail.com>
2023-12-03 12:53:37 -0500

    updated url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,12 +19,16 @@ export default function Document() {
         {/* <!-- Site Name --> */}
         <meta property="og:site_name" content="SiteBehaviour"></meta>
 
+        <meta property="og:image" content="https://qualaroo.com/blog/wp-content/uploads/2016/12/The-Ultimate-Guide-to-SEO-for-UX-Designers.jpg" />
+         <meta property="og:image:width" content="758" />
+         <meta property="og:image:height" content="335" />
+
         {/* <!-- Twitter --> */}
-        <meta property="twitter:card" content="summary_large_image"></meta>
+        {/* <meta property="twitter:card" content="summary_large_image"></meta>
         <meta property="twitter:url" content="https://sitebehaviour.com/" ></meta>
         <meta property="twitter:title" content="SiteBehaviour : Website Analytics Tool" ></meta>
         <meta property="twitter:description" content="Gain valuable insights into your website's performance. Discover heatmaps, session recordings, funnels, and filters to optimize your online presence." ></meta>
-        <meta property="twitter:image" content="/assets/hero-icon.png" ></meta>
+        <meta property="twitter:image" content="/assets/hero-icon.png" ></meta> */}
 
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
 
