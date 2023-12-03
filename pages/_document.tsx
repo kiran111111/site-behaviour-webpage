ebe9e61e438ae6225a0a993ba1b68c4773443717
Kirandeep <kiran111candy@gmail.com>
2023-12-03 13:05:33 -0500

    updating seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,9 +6,10 @@ export default function Document() {
       <Head>
          {/* <title>SiteBehaviour : Website Analytics Tool </title>
          <meta name="description" content="Gain valuable insights into your website's performance with our powerful analytics software tool. Discover heatmaps, session recordings, funnels, and filters to optimize your online presence." />
-         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-         <meta char-set="UTF-8"></meta>
+        
          <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+         <meta char-set="UTF-8"></meta>
          <link rel="apple-touch-icon" href="/assets/favicon.png" />
          <link rel="icon" href="/assets/favicon.png" />
 
