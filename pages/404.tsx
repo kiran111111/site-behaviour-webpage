c13e8465a0047a365ac65904defed9b4af315f7f
kirandeepk <kirandeepk@kirandeepks-MacBook-Pro.local>
2023-12-04 12:41:53 -0500

    updated 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,7 @@
 export default function Custom404(){
     return(
 
-        <section className="bg-white dark:bg-gray-900">
+        <section className="bg-white h-screen items-center dark:bg-gray-900 ">
             <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
                 <div className="mx-auto max-w-screen-sm text-center">
                     <h1 className="mb-4 text-7xl tracking-tight font-extrabold text-black lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
@@ -13,7 +13,6 @@ export default function Custom404(){
                 </div>   
             </div>
         </section>
-
 
     )
 }
