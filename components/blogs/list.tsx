a346d56e10e5a1d4f0e336d4691dd2757aa0bf8b
Kirandeep <kiran111candy@gmail.com>
2023-12-06 22:31:23 -0500

    updated heatmaps blogs

--- components/blogs/list.tsx
+++ components/blogs/list.tsx
@@ -127,6 +127,49 @@ export default function BlogList(){
                        </div>
 
                    </article> 
+
+                   <article className="p-6 grid md:grid-cols-3 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+                       
+                       <div className=" md:mr-6 md:mb-0 mb-6">
+                            <img className="h-full rounded-lg "
+                             src="/assets/blogs/heatmap.png" />
+                       </div>
+                       <div className="md:col-span-2">
+
+                               <div className="flex justify-between items-center mb-5 text-gray-400">
+                                   <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-zinc-200 dark:text-zinc-800">
+                                       NEW
+                                   </span>
+                                   <span className="text-sm">Dec 4, 2023</span>
+                               </div>
+                               <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                   <a href="/blogs/decoding-website-heatmaps">
+                                      Decoding Website Heat Maps
+                                      {/* : Understanding Visitor Behavior 
+                                      for Improved Online Success  */}
+                                     </a>
+                               </h2>
+                               <p className="mb-5 font-light text-gray-500 dark:text-gray-400">
+                                  Think of web analytics as your website's own detective. It's like having someone keep
+                                   an eye on your website and telling you everything about the people who visit - 
+                                   where they come from, what they like, and what they do.
+                               </p>
+                               <div className="flex justify-between items-center">
+                                   <div className="flex items-center space-x-4">
+                                       by {" "}   {/* <img className="w-7 h-7 rounded-full" src="" /> */}
+                                       <span  className="font-medium dark:text-white ml-2">
+                                           Kiran Kaur
+                                       </span>
+                                   </div>
+                                   <a href="/blogs/decoding-website-heatmaps" className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline">
+                                       Read more
+                                       <svg className="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                   </a>
+                               </div>
+
+                       </div>
+
+                   </article> 
                    
                 </div>  
             </div>
--- pages/blogs/decoding-website-heatmaps.tsx
+++ pages/blogs/decoding-website-heatmaps.tsx
@@ -57,7 +57,7 @@ export default function DecodingWebsiteHeatMaps() {
                                 </p>
                             </div>
                             <div className="">
-                                <img className="rounded-md shadow-lg " alt="web analytics" src="https://images.unsplash.com/photo-1504868584819-f8e8b4b6d7e3?q=80&w=2952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+                                <img className="rounded-md shadow-lg " alt="web analytics" src="/assets/features/feature-hero.png" />
                             </div>
                             <div className="dark:text-gray-800 ">
                                 <p>
